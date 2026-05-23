900de145d31039657edcce6eda152ad79f8ba3d4
Diogo <165814443+DiogoHonorioCamarinha@users.noreply.github.com>
2026-05-23 14:00:50 -0300

    Update AnimalScreen.js

--- components/AnimalScreen.js
+++ components/AnimalScreen.js
@@ -109,7 +109,7 @@ function AnimalScreen(){
 
                         <View style={{ borderRadius: 14, overflow: 'hidden' }}>
                             <Button
-                                title={editarId ? "Atualizar anúncio" : "Publicar anúncio"}
+                                title={editarId ? "Atualizar pet" : "Publicar pet"}
                                 onPress={salvarAnimal}
                                 color="#4f8d7a"
                             />
